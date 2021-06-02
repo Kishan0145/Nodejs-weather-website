54784c1401c2e23d51c3d1a60f245714eee31b45
Kishan Sharma <kishansharma0145@gmail.com>
2021-06-02 10:19:03 +0530

    Removing unecessary console.log calls

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-console.log("This is a test javascirpt file");
+
 document.querySelector("form").addEventListener("submit",(e)=>{
     e.preventDefault();
     const searchCity = document.querySelector("input").value;
